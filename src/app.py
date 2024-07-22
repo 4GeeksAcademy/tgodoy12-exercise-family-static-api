64903b720a1ae8bef9b8720815195374aa236ba5
tgodoy12 <112527761+tgodoy12@users.noreply.github.com>
2024-07-22 18:24:40 +0000

    class methods done and all endpoints added

--- src/app.py
+++ src/app.py
@@ -15,6 +15,7 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -25,18 +26,48 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# get all members
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
+    response_body = members
 
     return jsonify(response_body), 200
+
+# get member by id
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
+
+# add a member
+@app.route('/member', methods=['POST'])
+def add_a_member():
+    request_body = request.get_json()
+    if not request_body:
+        return jsonify({"msg": "No request body"}), 400
+    jackson_family.add_member(request_body)
+    
+    return jsonify("Member added"), 200
+
+# delete member by id
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_a_member(id):
+    try:
+        deleted = jackson_family.delete_member(id)
+        if deleted:
+            return jsonify({"done": True}), 200
+        else:
+            return jsonify({"done": False}), 400
+    except:
+        return jsonify({"msg": "Internal server error"}), 500
+
+      
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
